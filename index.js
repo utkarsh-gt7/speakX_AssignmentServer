b0d3ec859909710b603d8a8bf203b70cdb679f0e
Utkarsh Singh <116863760+utkarsh-gt7@users.noreply.github.com>
2025-01-26 22:04:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,7 @@ const connectDB = require("./config/db");
 const grpcServer = require("./grpc/server");
 require("dotenv").config();
 
-// Connect to MongoDB
 connectDB();
 console.log("Connected to MongoDB URI:", process.env.MONGO_URI);
 
-
-// Start gRPC server
 grpcServer.start();
