7f1becb8b651451ff962d05620bb5aef3270fafe
Utkarsh Singh <116863760+utkarsh-gt7@users.noreply.github.com>
2025-01-26 22:01:34 +0530

    Update grpcClient.js

--- client/grpcClient.js
+++ client/grpcClient.js
@@ -2,7 +2,6 @@ const grpc = require("@grpc/grpc-js");
 const protoLoader = require("@grpc/proto-loader");
 const path = require("path");
 
-// Load the proto file
 const protoPath = path.resolve(__dirname, "../proto/question.proto");
 const packageDefinition = protoLoader.loadSync(protoPath, {
   keepCase: true,
@@ -13,10 +12,8 @@ const packageDefinition = protoLoader.loadSync(protoPath, {
 });
 const questionProto = grpc.loadPackageDefinition(packageDefinition).question;
 
-// Create a gRPC client
 const client = new questionProto.QuestionService("localhost:50051", grpc.credentials.createInsecure());
 
-// Test GetQuestionsByTitle
 client.GetQuestionsByTitle({ title: "Rearrange" }, (err, response) => {
   if (err) {
     console.error("Error:", err.message);
@@ -25,12 +22,10 @@ client.GetQuestionsByTitle({ title: "Rearrange" }, (err, response) => {
   }
 });
 
-// Test GetQuestionsByType
 client.GetQuestionsByType({ type: "ANAGRAM" }, (err, response) => {
   if (err) {
     console.error("Error:", err.message);
   } else {
-    // Only log the first 10 questions
     const first10Questions = response.questions.slice(0, 10);
     console.log("First 10 Questions by Type:", first10Questions);
   }
